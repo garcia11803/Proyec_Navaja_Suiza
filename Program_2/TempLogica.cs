663facfa29c6426e00adb03377b9078259a85e37
garcia11803 <garcia11803@iesmarenostrum.com>
2017-05-10 13:28:57 +0200

    Creo las clases logicas de cada programa

---
+++ Program_2/TempLogica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyec_Navaja_Suiza.Temp
+{
+    public class TempLogica
+    {
+
+
+    }
+}
---
+++ Program_3/PalidromicaLogica.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyec_Navaja_Suiza.Palindromica
+{
+    public class PalidromicaLogica
+    {
+    }
+}
