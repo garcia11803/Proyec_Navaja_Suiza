7943fca1cd4265878281518268b382e60be22298
garcia11803 <garcia11803@iesmarenostrum.com>
2017-05-12 18:30:30 +0200

    Retoco color de fondo y botones del formulario principal

--- Navaja_Suiza.Designer.cs
+++ Navaja_Suiza.Designer.cs
@@ -36,48 +36,57 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(90, 53);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(141, 60);
             this.button1.TabIndex = 0;
             this.button1.Text = "Sumar numeros";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.bSumanumeros_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(90, 180);
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(90, 227);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(141, 75);
             this.button2.TabIndex = 1;
             this.button2.Text = "Temperatura ";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.bTemperatura_Click);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Location = new System.Drawing.Point(339, 53);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(132, 60);
             this.button3.TabIndex = 2;
             this.button3.Text = "Palabra Palindromica";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.bPalindromo_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(339, 180);
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(339, 227);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(132, 75);
             this.button4.TabIndex = 3;
             this.button4.Text = "Factorial de un numero";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.bFactorial_Click);
             // 
             // Navaja_Suiza
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(569, 422);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
